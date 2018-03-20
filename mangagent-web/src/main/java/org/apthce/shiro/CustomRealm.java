d7685a05dc1d5585d5ddbd496d83c622135cef7d
zhubo <1617074941@qq.com>
2018-03-20 15:06:15 +0800

    管理员、企业、子公司service提交

--- mangagent-web/src/main/java/org/apthce/shiro/CustomRealm.java
+++ mangagent-web/src/main/java/org/apthce/shiro/CustomRealm.java
@@ -12,13 +12,11 @@ import org.apache.shiro.authz.SimpleAuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
-import org.mangagent.dao.SysPermissionDao;
-import org.mangagent.dao.SysUserDao;
-import org.mangagent.pojo.SysUser;
+ 
 
 public class CustomRealm extends AuthorizingRealm {
    
-	private SysUserDao sysUserDao;
+	/*private SysUserDao sysUserDao;
     private SysPermissionDao sysPermissionDao;
 	public void setSysUserDao(SysUserDao sysUserDao) {
 		this.sysUserDao = sysUserDao;
@@ -26,29 +24,31 @@ public class CustomRealm extends AuthorizingRealm {
 
 	public void setSysPermissionDao(SysPermissionDao sysPermissionDao) {
 		this.sysPermissionDao = sysPermissionDao;
-	}
+	}*/
     /**
      * 授权
      */
 	@Override
 	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-		SysUser user=(SysUser)principals.getPrimaryPrincipal();
+		/*SysUser user=(SysUser)principals.getPrimaryPrincipal();
 		List<String>list=sysPermissionDao.getPermissionCodeByUserId(user.getId());
 	    SimpleAuthorizationInfo info=new SimpleAuthorizationInfo();
 	    info.addStringPermissions(list);
-		return info;
+		return info;*/
+		return null;
 	}
     /**
      * 认证
      */
 	@Override
 	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-		String principals=(String)token.getPrincipal(); //获取身份
+		/*String principals=(String)token.getPrincipal(); //获取身份
 		SysUser sysUser=sysUserDao.getByUsername(principals);
 		if(sysUser==null){
 			return null;
 		}
-		return new SimpleAuthenticationInfo(sysUser,sysUser.getPassword(),ByteSource.Util.bytes(sysUser.getSalt()),this.getName());
+		return new SimpleAuthenticationInfo(sysUser,sysUser.getPassword(),ByteSource.Util.bytes(sysUser.getSalt()),this.getName());*/
+		return null;
 	}
 
 }
